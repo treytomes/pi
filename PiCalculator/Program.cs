b3b4d58a9b54db4d8ab1263ca57defd86c7bc659
Trey Tomes <trey.tomes@gmail.com>
2025-10-23 12:30:34 -0500

    feat: initial commit

---
+++ PiCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PiCalculator;
+
+public static class Program
+{
+	/// <summary>
+	/// Calculate the value of π using François Viète's formula.
+	/// </summary>
+	public static void Main()
+	{
+		Console.WriteLine($"Math.PI    = {Math.PI}");
+
+		var product = 1.0;
+		var numerator = 0.0;
+
+		for (var counter = 0; counter < 30; counter++)
+		{
+			numerator = Math.Sqrt(2.0 + numerator);
+			product *= numerator / 2.0;
+		}
+
+		var pi = 2 / product;
+		Console.WriteLine($"Viète's PI = {pi}");
+	}
+}
