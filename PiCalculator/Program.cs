ac3c2a5abb226d8cfe8f426f8308e9dfcb6a7653
Trey Tomes <trey.tomes@gmail.com>
2025-10-28 13:00:57 -0500

    feat: Gregory-Leibniz's pi algorithm

--- PiCalculator/Program.cs
+++ PiCalculator/Program.cs
@@ -1,21 +1,60 @@
-﻿namespace PiCalculator;
+﻿using System.Collections.Concurrent;
+
+namespace PiCalculator;
 
 public static class Program
 {
 	// TODO: What is the correct epsilon value for a decimal?
-	const decimal DECIMAL_EPSILON = 0.00000000000000000000000000001M;
+	private const decimal DECIMAL_EPSILON = 0.00000000000000000000000000001M;
 
 	/// <summary>
 	/// Calculate the value of π using François Viète's formula.
 	/// </summary>
 	public static void Main()
 	{
-		Console.WriteLine($"Math.PI    = {Math.PI}");
+		Console.WriteLine($"Math.PI              = {Math.PI}");
 
-		var pi = VietePi(HeronSqrt);
-		Console.WriteLine($"Viète's PI = {pi}");
+		var pi0 = VietePi(HeronSqrt);
+		Console.WriteLine($"Viète's PI           = {pi0}");
+
+		var pi1 = GregoryLeibnizPi();
+		Console.WriteLine($"Gregory-Leibniz's PI = {pi1}");
 	}
 
+	private static decimal GregoryLeibnizPi()
+	{
+		ConcurrentStack<decimal> values = new();
+
+		// var options = new ParallelOptions()
+		// {
+		// 	MaxDegreeOfParallelism = int.MaxValue,
+		// };
+
+		var result = Parallel.For(0, 10_000_000_000, iter =>
+		{
+			var numerator = 4M;
+			var sign = (iter % 2M == 0M) ? 1M : -1M;
+			var denominator = 2M * iter + 1M;
+
+			var pi0 = sign * numerator / denominator;
+			// if (pi0 == pi)
+			// {
+			// 	return pi0;
+			// }
+
+			// pi = pi0;
+
+			// iters++;
+			// Console.WriteLine($"{iter}: {pi0}");
+
+			values.Push(pi0);
+		});
+
+		var pi = values.Sum();
+		return pi;
+	} 
+
+	/// <remarks>47 iterations to max out the decimal datatype.</remarks>
 	private static decimal VietePi(Func<decimal, decimal> sqrt)
 	{
 		var p0 = 1.0M;
@@ -30,11 +69,6 @@ public static class Program
 			{
 				break;
 			}
-			// if (Math.Abs(p1 - p0) <= decimalEpsilon)
-			// {
-			// 	p0 = p1;
-			// 	break;
-			// }
 			p0 = p1;
 		}
 
